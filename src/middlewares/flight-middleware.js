ce27d76281c70145a19ece69287e98bf17a7fbd5
Pavan Kumar <32819459+kamarajugadda-pavan-kumar@users.noreply.github.com>
2024-10-07 16:53:08 +0530

    Update flight-middleware.js

--- src/middlewares/flight-middleware.js
+++ src/middlewares/flight-middleware.js
@@ -1,6 +1,7 @@
 const { StatusCodes } = require("http-status-codes");
 const { ErrorResponse } = require("../utils/common");
 const AppError = require("../utils/errors/app-error");
+const { flightBookingEnums } = require("../utils/common/enums");
 
 function validateCreateRequest(req, res, next) {
   let fields = [
@@ -35,4 +36,42 @@ function validateCreateRequest(req, res, next) {
   next();
 }
 
-module.exports = { validateCreateRequest };
+function validateModifyAvailableSeatsCount(req, res, next) {
+  let error = new AppError(
+    "Something went wrong while updating seat count",
+    StatusCodes.BAD_REQUEST,
+    null,
+    []
+  );
+
+  if (!req.body.noOfSeats) {
+    error.details.push("noOfSeats field missing");
+  }
+
+  if (
+    (req.body.noOfSeats && isNaN(parseInt(req.body.noOfSeats))) ||
+    parseInt(req.body.noOfSeats) < 0
+  ) {
+    error.details.push("noOfSeats is not a valid Integer");
+  }
+
+  if (!req.body.action) {
+    error.details.push("action field missing");
+  }
+
+  if (req.body.action && !flightBookingEnums.includes(req.body.action)) {
+    error.details.push("Value provided in action is invalid");
+  }
+
+  if (error.details.length) {
+    ErrorResponse.error = {
+      explanation: error.explanation,
+      details: error.details,
+    };
+    return res.status(error.statusCode).json(ErrorResponse);
+  }
+
+  next();
+}
+
+module.exports = { validateCreateRequest, validateModifyAvailableSeatsCount };
